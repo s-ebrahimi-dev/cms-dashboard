f4258e96621d4ae2944410310d37cd30355ca114
Saeed Ebrahimi <s.ebrahimi.dev@gmail.com>
2026-09-05 17:41:26 +0330

    fix register.html

--- scripts/components/component-Loader.js
+++ scripts/components/component-Loader.js
@@ -4,7 +4,13 @@ export async function loadComponent(id, path) {
 
     const res = await fetch(path);
 
+    console.log("Loading component:", path);
+    console.log("Status:", res.status);
+    console.log("Content-Type:", res.headers.get("content-type"));
+
     const html = await res.text();
+
+    console.log(html.slice(0, 100));
 
     container.innerHTML = html;
 }
